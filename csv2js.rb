3a80f5088866a0727bbad6015b2734e255851119
YASUI Kentarow <yasuikentarow@gmail.com>
2022-01-30 08:35:16 +0900

    スキル一覧更新

--- csv2js.rb
+++ csv2js.rb
@@ -9,27 +9,30 @@ CSV.foreach(ARGV[0]) { |row|
   else
     skill = Hash.new
     row.each_with_index { |cell, i|
+      key = keys[i]
       if cell
-        skill[keys[i]] = cell
+        skill[key] = (value = skill[key]) ? (Array(value) + [ cell ]) : cell
       end
     }
-    ( 1 .. 5 ).each { |rank|
-      if max = skill["R#{rank}"]
-        min = skill["R#{rank}+"]
-        args = [
-          skill['rarity'], 
-          skill['name'], 
-          skill['condition'] || "", 
-          skill['cost'].to_i, 
-          rank, 
-          max.to_f, 
-          min ? min.to_f : 0
-        ].map { |arg|
-          arg.is_a?(String) ? %("#{arg}") : arg.to_s
-        }        
-        skills << "new Skill(#{args.join(',')})"
-      end
+    Array(skill['condition']).each { |condition|
+      ( 1 .. 5 ).each { |rank|
+        if max = skill["R#{rank}"]
+          min = skill["R#{rank}+"]
+          args = [
+            skill['rarity'], 
+            skill['name'], 
+            condition, 
+            skill['cost'].to_i, 
+            rank, 
+            max.to_f, 
+            min ? min.to_f : 0
+          ].map { |arg|
+            arg.is_a?(String) ? %("#{arg}") : arg.to_s
+          }        
+          skills << "new Skill(#{args.join(',')})"
+        end
+      }
     }
   end
 }
-puts("const Skills = [ #{skills.join(',')} ];")
+puts(%{const Skills = [\n\t#{skills.join(",\n\t")}\n];})
